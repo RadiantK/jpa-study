f2628c28fc4998d5df9bd8c6aeed2c2ba1c0e77a
RadiantK <pps8853@gmail.com>
2022-10-18 01:25:41 +0900

    jpa queryMethod 사용
    jpa queryMethod를 다양한 방식으로 사용

--- src/main/java/com/jpa/bookmanager/repository/UserRepository.java
+++ src/main/java/com/jpa/bookmanager/repository/UserRepository.java
@@ -1,8 +1,81 @@
 package com.jpa.bookmanager.repository;
 
 import com.jpa.bookmanager.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String name); // 이름으로 레코드를 하나 얻어옴
+
+    User findByEmail(String email);
+
+    User getByEmail(String email);
+
+    User readByEmail(String email);
+
+    User queryByEmail(String email);
+
+    User searchByEmail(String email);
+
+    User streamByEmail(String emaill);
+
+    User findUserByEmail(String email);
+
+    User findSomethingByEmail(String email);
+
+    User findFirst1ByName(String name); //List<User> findFirst1ByName(String name);
+
+    User findTop1ByName(String name); //List<User> findTop1ByName(String name);
+
+    User findByEmailAndName(String email, String name);
+
+    List<User> findByEmailOrName(String email, String name);
+
+    List<User> findByCreatedAtAfter(LocalDateTime localDateTime); // 특정날짜보다 큰 날짜
+    List<User> findByCreatedAtBefore(LocalDateTime localDateTime); // 특정날짜보다 작은 날짜
+
+    List<User> findByIdAfter(Long id); // 특정 아이디보다 큰 값
+
+    List<User> findByCreatedAtGreaterThan(LocalDateTime localDateTime);
+
+    List<User> findByCreatedAtGreaterThanEqual(LocalDateTime localDateTime);
+
+    List<User> findByCreatedAtBetween(LocalDateTime from, LocalDateTime to);
+
+    List<User> findByIdBetween(Long id1, Long id2);
+
+    List<User> findByIdGreaterThanEqualAndIdLessThanEqual(Long id1, Long id2);
+
+    List<User> findByIdIsNotNull();
+
+    List<User> findByNameIn(List<String> names);
+
+    List<User> findByNameStartingWith(String name);
+
+    List<User> findByNameEndingWith(String name);
+
+    List<User> findByNameContains(String name);
+
+    List<User> findByNameLike(String name);
+
+    List<User> findByNameIs(String name); // is: 코드 가독성 용도
+
+    // paging and sorting
+    List<User> findTop1ByNameOrderByIdDesc(String name);
+
+    List<User> findTopByNameOrderByIdDesc(String name);
+
+    List<User> findFirstByNameOrderByIdDescEmailAsc(String name);
+
+    List<User> findFirstByName(String name, Sort sort);
+
+    Page<User> findByName(String name, Pageable pageable);
 
 }
--- src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -101,4 +101,56 @@ class UserRepositoryTest {
         userRepository.save(user);
     }
 
+    @Test
+    void select() {
+//        log.info("findByName={}", userRepository.findByName("mark"));
+//
+//        System.out.println("findByEmail: " + userRepository.findByEmail("kang@fastcampus.com"));
+//        System.out.println("getByEmail: " + userRepository.getByEmail("kang@fastcampus.com"));
+//        System.out.println("readByEmail: " + userRepository.readByEmail("kang@fastcampus.com"));
+//        System.out.println("queryByEmail: " + userRepository.queryByEmail("kang@fastcampus.com"));
+//        System.out.println("searchByEmail: " + userRepository.searchByEmail("kang@fastcampus.com"));
+//        System.out.println("streamByEmail: " + userRepository.streamByEmail("kang@fastcampus.com"));
+//        System.out.println("findUserByEmail: " + userRepository.findUserByEmail("kang@fastcampus.com"));
+//        System.out.println("findSomethingUserByEmail: " + userRepository.findSomethingByEmail("kang@fastcampus.com"));
+//
+//        System.out.println("findFirst1ByName: " + userRepository.findFirst1ByName("kang"));
+//        System.out.println("findTop1ByName: " + userRepository.findTop1ByName("kang"));
+
+//        System.out.println("findByEmailAndName: " + userRepository.findByEmailAndName("kang@fastcampus.com", "kang"));
+//        System.out.println("findByEmailOrName: " + userRepository.findByEmailOrName("kang@fastcampus.com", "mark"));
+//        System.out.println("findByCreatedAtAfter: " + userRepository.findByCreatedAtAfter(LocalDateTime.now().minusDays(1L)));
+//        System.out.println("findByIdAfter: " + userRepository.findByIdAfter(3L));
+//        System.out.println("findByCreatedAtGreaterThan: " + userRepository.findByCreatedAtGreaterThan(LocalDateTime.now().minusDays(1L)));
+//        System.out.println("findByCreatedAtGreaterThanEqual: " + userRepository.findByCreatedAtGreaterThanEqual(LocalDateTime.now().minusDays(1L)));
+//
+//        System.out.println("findByCreatedAtBetween: " + userRepository.findByCreatedAtBetween(LocalDateTime.now().minusDays(1L), LocalDateTime.now().plusDays(1L)));
+//        System.out.println("findByIdBetween: " + userRepository.findByIdBetween(1L, 3L));
+//        System.out.println("findByIdGreaterThanEqualAndIdLessThanEqual: " + userRepository.findByIdGreaterThanEqualAndIdLessThanEqual(1L, 3L));
+
+//        System.out.println("findByIdIsNotNull: " + userRepository.findByIdIsNotNull());
+//        System.out.println("findByNameIn: " + userRepository.findByNameIn(List.of("kang", "mark")));
+
+//        System.out.println("findByNameStartingWith: " + userRepository.findByNameStartingWith("kan"));
+//        System.out.println("findByNameEndingWith: " + userRepository.findByNameEndingWith("ang"));
+//        System.out.println("findByNameContains: " + userRepository.findByNameContains("an"));
+//        System.out.println("findByNameLike: " + userRepository.findByNameLike("%"+"an"+"%"));
+//        System.out.println("findByNameIs: " + userRepository.findByNameIs("kang"));
+    }
+
+    @Test
+    void pagingAndSortingTest() {
+
+        //sorting
+//        System.out.println("findTop1ByName: " + userRepository.findTop1ByName("kang"));
+//        System.out.println("findTop1ByNameOrderByIdDesc: " + userRepository.findTop1ByNameOrderByIdDesc("kang"));
+//        System.out.println("findTopByNameOrderByIdDesc: " + userRepository.findTopByNameOrderByIdDesc("kang"));
+//        System.out.println("findFirstByNameOrderByIdDescEmailAsc: " + userRepository.findFirstByNameOrderByIdDescEmailAsc("kang"));
+//        System.out.println("findFirstByNameWithSortParams: " + userRepository.findFirstByName("kang", Sort.by(Sort.Order.desc("id"), Sort.Order.desc("email"))));
+
+        // paging
+        System.out.println("findByNameWithPaging: " + userRepository.findByName("kang", PageRequest.of(0, 1, Sort.by(Sort.Order.desc("id")))).getContent());
+
+    }
+
 }
